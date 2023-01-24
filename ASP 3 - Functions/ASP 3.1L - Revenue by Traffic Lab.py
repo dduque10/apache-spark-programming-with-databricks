08733012845618192f7b524d49bc3f460f223f74
dduque10 <37351039+dduque10@users.noreply.github.com>
2023-01-24 10:47:20 +0100

    Complete agg and math lab

--- ASP 3 - Functions/ASP 3.1L - Revenue by Traffic Lab.py
+++ ASP 3 - Functions/ASP 3.1L - Revenue by Traffic Lab.py
@@ -53,9 +53,9 @@ display(df)
 
 # COMMAND ----------
 
-# TODO
+from pyspark.sql.functions import sum, avg
 
-traffic_df = (df.FILL_IN
+traffic_df = (df.groupBy("traffic_source").agg(sum("revenue").alias("total_rev"), avg("revenue").alias("avg_rev"))
 )
 
 display(traffic_df)
@@ -83,8 +83,9 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-top_traffic_df = (traffic_df.FILL_IN
+from pyspark.sql.functions import col
+
+top_traffic_df = (traffic_df.sort(col("total_rev").desc()).limit(3)
 )
 display(top_traffic_df)
 
@@ -110,8 +111,9 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-final_df = (top_traffic_df.FILL_IN
+final_df = (top_traffic_df
+            .withColumn("avg_rev", (col("avg_rev")*100).cast("long")/100)
+            .withColumn("total_rev", (col("total_rev")*100).cast("long")/100)
 )
 
 display(final_df)
@@ -136,8 +138,11 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-bonus_df = (top_traffic_df.FILL_IN
+from pyspark.sql.functions import round
+
+bonus_df = (top_traffic_df
+            .withColumn("avg_rev", round("avg_rev", 2))
+            .withColumn("total_rev", round("total_rev", 2))
 )
 
 display(bonus_df)
@@ -160,8 +165,11 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-chain_df = (df.FILL_IN
+chain_df = (df
+            .groupBy("traffic_source").agg(sum("revenue").alias("total_rev"), avg("revenue").alias("avg_rev"))
+            .sort(col("total_rev").desc()).limit(3)
+            .withColumn("avg_rev", round("avg_rev", 2))
+            .withColumn("total_rev", round("total_rev", 2))
 )
 
 display(chain_df)
