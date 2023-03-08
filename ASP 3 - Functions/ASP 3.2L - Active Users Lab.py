8f0e7a4fc503878660f9b5262695d036b65ff563
dduque10 <dduque10>
2023-03-08 10:25:51 +0000

    Add solution to Dates Lab

--- ASP 3 - Functions/ASP 3.2L - Active Users Lab.py
+++ ASP 3 - Functions/ASP 3.2L - Active Users Lab.py
@@ -46,8 +46,11 @@ display(df)
 
 # COMMAND ----------
 
-# TODO
-datetime_df = (df.FILL_IN
+from pyspark.sql.functions import to_date
+
+datetime_df = (df
+               .withColumn("ts", (col("ts")/1e6).cast("timestamp"))
+               .withColumn("date", to_date(col("ts")))
 )
 display(datetime_df)
 
@@ -90,8 +93,9 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-active_users_df = (datetime_df.FILL_IN
+from pyspark.sql.functions import approx_count_distinct
+
+active_users_df = (datetime_df.groupBy("date").agg(approx_count_distinct("user_id").alias("active_users"))
 )
 display(active_users_df)
 
@@ -130,8 +134,12 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-active_dow_df = (active_users_df.FILL_IN
+from pyspark.sql.functions import date_format, avg
+
+active_dow_df = (active_users_df
+                 .withColumn("day", date_format(col("date"), "E"))
+                 .groupBy("day")
+                 .agg(avg("active_users").alias("avg_users"))
 )
 display(active_dow_df)
 
